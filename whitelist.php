cd0b4db2f109b8628a8f2934f9177c7bf1461770
Boris Radulov <sc20br@leeds.ac.uk>
2022-03-25 23:59:05 +0200

    polygon

--- whitelist.php
+++ whitelist.php
@@ -1,29 +1,30 @@
 <?php
-	if (!isset($_POST["email"]) || !isset($_POST["eth"])) {
-		die('missing args');
-	}
+    if (!isset($_POST["email"]) || !isset($_POST["eth"])) {
+            die('missing args');
+    }
 
-	if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) {
-		die('bad email')l
-	}
+    if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+            die('bad email');
+    }
 
-	$eth_pattern = "/^0x[a-fA-F0-9]{40}$/";
-	if (preg_match_all($eth_pattern, $_POST["eth"]) <= 0) {
-		die('bad addr');
-	}
+    $eth_pattern = "/^0x[a-fA-F0-9]{40}$/";
+    if (preg_match_all($eth_pattern, $_POST["eth"]) <= 0) {
+            die('bad addr');
+    }
 
-	try{
-		$conn = new PDO("mysql:host=localhost;dbname=backroom", "root", "root");
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	catch(PDOException $e) {
-		die('cant connect to db');
-	}
+    try{
+            $conn = new PDO("mysql:host=localhost;dbname=backroom", "root", "root");
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $e) {
 
-	$email = $_POST["email"];
-	$eth = $_POST["eth"];
-	$twitter = $_POST["twitter"];
-	$sql = "INSERT INTO whitelist (email, eth, twitter) VALUES (?, ?, ?)"
-	$stmt = $pdo->prepare($sql);
-	$stmt->execute([$email, $eth, $twitter]);
+            die('cant connect to db');
+    }
+
+    $email = $_POST["email"];
+    $eth = $_POST["eth"];
+    $twitter = $_POST["twitter"];
+
+    $sql = "INSERT INTO whitelist (email, eth, twitter) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$email, $eth, $twitter]);
 ?>
-
