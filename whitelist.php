d78297507f73e40df01def40afe10001bfe6f49a
Boris Radulov <sc20br@leeds.ac.uk>
2022-03-26 00:09:47 +0200

    form

--- whitelist.php
+++ whitelist.php
@@ -1,23 +1,26 @@
 <?php
     if (!isset($_POST["email"]) || !isset($_POST["eth"])) {
-            die('missing args');
+            echo 'Please fill out the form!';
+            exit(400);
     }
 
     if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-            die('bad email');
+            echo 'Invalid email!';
+            exit(400);
     }
 
     $eth_pattern = "/^0x[a-fA-F0-9]{40}$/";
     if (preg_match_all($eth_pattern, $_POST["eth"]) <= 0) {
-            die('bad addr');
+            echo 'Invalid Polygon address!';
+            exit(400);
     }
 
     try{
             $conn = new PDO("mysql:host=localhost;dbname=backroom", "root", "root");
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }catch(PDOException $e) {
-
-            die('cant connect to db');
+        echo 'Something went wrong... Please try again later.';
+        exit(400);
     }
 
     $email = $_POST["email"];
@@ -26,5 +29,8 @@
 
     $sql = "INSERT INTO whitelist (email, eth, twitter) VALUES (?, ?, ?)";
     $stmt = $conn->prepare($sql);
-    $stmt->execute([$email, $eth, $twitter]);
+    if(!$stmt->execute([$email, $eth, $twitter])) {
+        echo 'You can only register for the whitelist once.';
+        exit(400);
+    }
 ?>
